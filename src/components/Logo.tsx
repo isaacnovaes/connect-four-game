64280df1a57cfaa75d175dcbc0071cd46c4ce987
Isaac Novaes <isaac.novaes@hotmail.com>
2025-06-05 14:48:42 +0200

    Create Logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,43 @@
+const Logo = () => {
+    return (
+        <svg
+            height='61px'
+            version='1.1'
+            viewBox='0 0 58 61'
+            width='58px'
+            xmlns='http://www.w3.org/2000/svg'
+            xmlnsXlink='http://www.w3.org/1999/xlink'
+        >
+            <title>logo</title>
+            <g fill='none' fillRule='evenodd' id='Designs' stroke='none' strokeWidth='1'>
+                <g id='assets' transform='translate(-432.000000, -742.000000)'>
+                    <g id='Group-24' transform='translate(211.000000, 160.000000)'>
+                        <g id='logo' transform='translate(221.000000, 582.000000)'>
+                            <g fill='#000000' id='Group-23'>
+                                <circle cx='13' cy='13' id='Oval-Copy-47' r='13' />
+                                <circle cx='45' cy='13' id='Oval-Copy-51' r='13' />
+                                <circle cx='13' cy='45' id='Oval-Copy-53' r='13' />
+                                <circle cx='45' cy='45' id='Oval-Copy-52' r='13' />
+                            </g>
+                            <g
+                                fill='#000000'
+                                id='Group-23-Copy'
+                                transform='translate(0.000000, 3.000000)'
+                            >
+                                <circle cx='13' cy='13' id='Oval-Copy-47' r='13' />
+                                <circle cx='45' cy='13' id='Oval-Copy-51' r='13' />
+                                <circle cx='13' cy='45' id='Oval-Copy-53' r='13' />
+                                <circle cx='45' cy='45' id='Oval-Copy-52' r='13' />
+                            </g>
+                            <circle cx='13' cy='13' fill='#FD6687' id='Oval-Copy-11' r='10' />
+                            <circle cx='45' cy='45' fill='#FD6687' id='Oval-Copy-25' r='10' />
+                            <circle cx='45' cy='13' fill='#FFCE67' id='Oval-Copy-23' r='10' />
+                            <circle cx='13' cy='45' fill='#FFCE67' id='Oval-Copy-24' r='10' />
+                        </g>
+                    </g>
+                </g>
+            </g>
+        </svg>
+    );
+};
+export default Logo;
