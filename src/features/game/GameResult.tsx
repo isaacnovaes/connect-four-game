370602fa65e867d684fdde55c6660d7179e17851
Isaac Novaes <isaac.novaes@hotmail.com>
2025-06-18 16:34:10 +0200

    Use CpuIcon

--- src/features/game/GameResult.tsx
+++ src/features/game/GameResult.tsx
@@ -1,3 +1,4 @@
+import CpuIcon from '../../components/icons/CpuIcon';
 import PlayerOneIcon from '../../components/icons/PlayerOneIcon';
 import PlayerTwoIcon from '../../components/icons/PlayerTwoIcon';
 import { useAppSelector } from '../../store/hooks';
@@ -9,7 +10,7 @@ const GameResult = () => {
     const playerTwoPoints = runs.filter((r) => r.player === 2).length * 4;
 
     return (
-        <div className='tablet:max-w-[745px] mx-auto mb-[50px] flex h-[81px] items-center justify-between px-9'>
+        <div className='tablet:max-w-[650px] mx-auto mb-[50px] flex h-[81px] items-center justify-between px-9'>
             <div className='shadow-box tablet:flex-row tablet:p-10 tablet:basis-[272px] relative flex h-[81px] basis-[142px] flex-col items-center justify-between rounded-[20px] border-2 border-black bg-white py-[10px]'>
                 <span className='text-xs uppercase'>{isCpuMode ? 'You' : 'Player 1'}</span>
                 <span className='text-[32px]'>{playerOnePoints}</span>
@@ -18,7 +19,11 @@ const GameResult = () => {
             <div className='shadow-box tablet:flex-row tablet:p-10 tablet:basis-[272px] relative flex h-[81px] basis-[142px] flex-col items-center justify-between rounded-[20px] border-2 border-black bg-white py-[10px]'>
                 <span className='text-xs uppercase'>{isCpuMode ? 'CPU' : 'Player 1'}</span>
                 <span className='text-[32px]'>{playerTwoPoints}</span>
-                <PlayerTwoIcon className='absolute top-[10px] -right-[30px]' />
+                {isCpuMode ? (
+                    <CpuIcon className='absolute top-[10px] -right-[30px]' />
+                ) : (
+                    <PlayerTwoIcon className='absolute top-[10px] -right-[30px]' />
+                )}
             </div>
         </div>
     );
