e66058271396845873d8133025f6f562276c3684
Isaac Novaes <isaac.novaes@hotmail.com>
2025-06-20 13:38:04 +0200

    Add BottomBg and GameResult animation

--- src/features/game/BottomBg.tsx
+++ src/features/game/BottomBg.tsx
@@ -1,13 +1,19 @@
+import { motion } from 'motion/react';
 import { useAppSelector } from '../../store/hooks';
+import { selectIsTabletBreakPoint } from './resizeSlice';
 
 const BottomBg = () => {
     const winnerPlayer = useAppSelector((s) => s.board.winnerPlayer);
+    const isTabletBreakPoint = useAppSelector(selectIsTabletBreakPoint);
 
     const bg = winnerPlayer ? (winnerPlayer === 1 ? 'bg-pink' : 'bg-yellow') : 'bg-purple-dark';
 
     return (
-        <div
+        <motion.div
+            animate={{ height: isTabletBreakPoint ? 234 : 236 }}
             className={`${bg} tablet:h-[234px] absolute bottom-0 h-[236px] w-full rounded-t-[60px] transition-colors`}
+            initial={{ height: 0 }}
+            transition={{ type: 'spring', bounce: 0.5, duration: 0.8 }}
         />
     );
 };
--- src/features/game/GameResult.tsx
+++ src/features/game/GameResult.tsx
@@ -1,3 +1,4 @@
+import { motion } from 'motion/react';
 import CpuIcon from '../../components/icons/CpuIcon';
 import PlayerOneIcon from '../../components/icons/PlayerOneIcon';
 import PlayerTwoIcon from '../../components/icons/PlayerTwoIcon';
@@ -9,17 +10,26 @@ const GameResult = () => {
     const playerOnePoints = runs.filter((r) => r.player === 1).length * 4;
     const playerTwoPoints = runs.filter((r) => r.player === 2).length * 4;
 
-    // set desktop display
     return (
         <div className='tablet:max-w-[650px] desktop:max-w-[1200px] desktop:left-[5%] desktop:right-[5%] desktop:top-[300px] desktop:absolute mx-auto mb-[50px] flex h-[81px] items-center justify-between px-9'>
-            <div className='shadow-box tablet:flex-row tablet:p-10 desktop:flex-col desktop:h-[150px] desktop:w-auto desktop:basis-auto tablet:basis-[272px] relative flex h-[81px] basis-[142px] flex-col items-center justify-between rounded-[20px] border-2 border-black bg-white py-[10px]'>
+            <motion.div
+                animate={{ scale: 1, rotate: 0 }}
+                className='shadow-box tablet:flex-row tablet:p-10 desktop:flex-col desktop:h-[150px] desktop:w-auto desktop:basis-auto tablet:basis-[272px] relative flex h-[81px] basis-[142px] flex-col items-center justify-between rounded-[20px] border-2 border-black bg-white py-[10px]'
+                initial={{ scale: 0, rotate: 90 }}
+                transition={{ type: 'spring', bounce: 0.5, duration: 0.8, delay: 0.2 }}
+            >
                 <span className='desktop:text-s text-xs uppercase'>
                     {isCpuMode ? 'You' : 'Player 1'}
                 </span>
                 <span className='desktop:text-l text-[32px]'>{playerOnePoints}</span>
                 <PlayerOneIcon className='desktop:-top-6 desktop:left-1/2 desktop:-translate-x-1/2 absolute top-[10px] -left-[30px]' />
-            </div>
-            <div className='shadow-box tablet:flex-row tablet:p-10 desktop:flex-col desktop:h-[150px] desktop:w-auto desktop:basis-auto tablet:basis-[272px] relative flex h-[81px] basis-[142px] flex-col items-center justify-between rounded-[20px] border-2 border-black bg-white py-[10px]'>
+            </motion.div>
+            <motion.div
+                animate={{ scale: 1, rotate: 0 }}
+                className='shadow-box tablet:flex-row tablet:p-10 desktop:flex-col desktop:h-[150px] desktop:w-auto desktop:basis-auto tablet:basis-[272px] relative flex h-[81px] basis-[142px] flex-col items-center justify-between rounded-[20px] border-2 border-black bg-white py-[10px]'
+                initial={{ scale: 0, rotate: 90 }}
+                transition={{ type: 'spring', bounce: 0.5, duration: 0.8, delay: 0.4 }}
+            >
                 <span className='desktop:text-s text-xs uppercase'>
                     {isCpuMode ? 'CPU' : 'Player 1'}
                 </span>
@@ -29,7 +39,7 @@ const GameResult = () => {
                 ) : (
                     <PlayerTwoIcon className='desktop:-top-6 desktop:left-1/2 desktop:-translate-x-1/2 absolute top-[10px] -right-[30px]' />
                 )}
-            </div>
+            </motion.div>
         </div>
     );
 };
